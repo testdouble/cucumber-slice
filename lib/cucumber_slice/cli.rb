12846897ca4480e08310b56d3f4b1bbe3ca1d65b
Justin Searls <searls@gmail.com>
2013-03-28 15:19:39 -0400

    Change --from to --since

--- lib/cucumber_slice/cli.rb
+++ lib/cucumber_slice/cli.rb
@@ -9,20 +9,29 @@ module CucumberSlice
       thinks should be run based on the current changeset of files since the
       previous passing run.
 
-      > $ cucumber-slice list --from a3989b6
+      > $ cucumber-slice list --since a3989b6
+
+      If the `since` option is not supplied, then HEAD is assumed and uncommitted
+      changes in your working
+
+      > $ cucumber-slice list
 
       In the event you don't want to filter out any features (perhaps following an
       unstable build), you can use the `--all` option to prevent filtering
 
-      > $ cucumber-slice list --from a3989b6 --all true
+      > $ cucumber-slice list --since a3989b6 --all true
+
+      If the features you wish to filter are anywhere but <git_root>/features, you can specify them:
+
+      > $ cucumber-slice list --since a3989b6 --features "project/features"
 
     LONGDESC
-    option :from, :type => :string, :required => true, :banner => "REV"
+    option :since, :type => :string, :default => "HEAD", :banner => "REV"
     option :all, :type => :boolean, :default => false
     option :features, :default => "features", :aliases => ["f"]
     def list
       require 'cucumber_slice/lists_features'
-      features = ListsFeatures.new(options[:features], options[:from], !!options[:all])
+      features = ListsFeatures.new(options[:features], options[:since], !!options[:all])
       puts features.list
     end
 
